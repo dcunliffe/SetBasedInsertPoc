bbc7fefa3b2996e36d4244af5a885e2da07a44f3
ALPHA\david.cunliffe <david.cunliffe2@nhs.net>
2024-05-24 15:56:44 +0100

    Add project files.

---
+++ SetbasedInsertPoc/DemoArtefacts.sql
@@ -0,0 +1,46 @@
+﻿create type dbo.AddressType as table
+(
+    [UPRN]                    NVARCHAR (MAX)   NULL,
+    [UPSN]                    NVARCHAR (MAX)   NULL,
+    [OrganisationName]        NVARCHAR (MAX)   NULL,
+    [DepartmentName]          NVARCHAR (MAX)   NULL,
+    [SubBuildingName]         NVARCHAR (MAX)   NULL,
+    [BuildingName]            NVARCHAR (MAX)   NULL,
+    [BuildingNumber]          NVARCHAR (MAX)   NULL,
+    [DependentThoroughfare]   NVARCHAR (MAX)   NULL,
+    [Thoroughfare]            NVARCHAR (MAX)   NULL,
+    [DoubleDependentLocality] NVARCHAR (MAX)   NULL,
+    [DependentLocality]       NVARCHAR (MAX)   NULL,
+    [PostTown]                NVARCHAR (MAX)   NULL,
+    [PostalAddress]           NVARCHAR (MAX)   NULL,
+    [PostCode]                NVARCHAR (MAX)   NULL,
+    [LibPostalData]           NVARCHAR (MAX)   NULL
+);
+GO
+Create Procedure dbo.insertAddress
+    @rows AddressType READONLY
+as
+begin
+    insert into Addresses
+    select * from @rows;
+end
+GO
+CREATE TABLE [dbo].[Addresses] (
+    [Id]                      int IDENTITY(1,1),
+    [UPRN]                    NVARCHAR (MAX)   NULL,
+    [UPSN]                    NVARCHAR (MAX)   NULL,
+    [OrganisationName]        NVARCHAR (MAX)   NULL,
+    [DepartmentName]          NVARCHAR (MAX)   NULL,
+    [SubBuildingName]         NVARCHAR (MAX)   NULL,
+    [BuildingName]            NVARCHAR (MAX)   NULL,
+    [BuildingNumber]          NVARCHAR (MAX)   NULL,
+    [DependentThoroughfare]   NVARCHAR (MAX)   NULL,
+    [Thoroughfare]            NVARCHAR (MAX)   NULL,
+    [DoubleDependentLocality] NVARCHAR (MAX)   NULL,
+    [DependentLocality]       NVARCHAR (MAX)   NULL,
+    [PostTown]                NVARCHAR (MAX)   NULL,
+    [PostalAddress]           NVARCHAR (MAX)   NULL,
+    [PostCode]                NVARCHAR (MAX)   NULL,
+    [LibPostalData]           NVARCHAR (MAX)   NULL,
+    CONSTRAINT [PK_Addresses] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
---
+++ SetbasedInsertPoc/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Data.SqlTypes;
+using System.Threading;
+using System.Data;
+using System.Diagnostics;
+using System.Data.SqlClient;
+
+internal class Program
+{
+    private static async Task AsyncMain()
+    {
+        var connection = new SqlConnection("Server=(localdb)\\MSSQLLocalDB;Database=UPRN;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+        await connection.OpenAsync();
+
+        var dataTable = new DataTable();
+
+        dataTable.Columns.Add("UPRN");
+        dataTable.Columns.Add("UPSN");
+        dataTable.Columns.Add("OrganisationName");
+        dataTable.Columns.Add("DepartmentName");
+        dataTable.Columns.Add("SubBuildingName");
+        dataTable.Columns.Add("BuildingName");
+        dataTable.Columns.Add("BuildingNumber");
+        dataTable.Columns.Add("DependentThoroughfare");
+        dataTable.Columns.Add("Thoroughfare");
+        dataTable.Columns.Add("DoubleDependentLocality");
+        dataTable.Columns.Add("DependentLocality");
+        dataTable.Columns.Add("PostTown");
+        dataTable.Columns.Add("PostalAddress");
+        dataTable.Columns.Add("PostCode");
+        dataTable.Columns.Add("LibPostalData");
+
+        var timer = new Stopwatch();
+
+        timer.Start();
+
+        Console.WriteLine(timer.Elapsed.ToString());
+
+        for (var i = 0; i < 1000; i++)
+        {
+            dataTable.Rows.Add(
+            "UPRN",
+            "UPSN",
+            "OrganisationName",
+            "DepartmentName",
+            "SubBuildingName",
+            "BuildingName",
+            "BuildingNumber",
+            "DependentThoroughfare",
+            "Thoroughfare",
+            "DoubleDependentLocality",
+            "DependentLocality",
+            "PostTown",
+            "PostalAddress",
+            "PostCode",
+            "LibPostalData");
+        }
+
+        SqlCommand command = connection.CreateCommand();
+        command.CommandText = "dbo.insertAddress";
+        command.CommandType = CommandType.StoredProcedure;
+
+        command.Parameters.Add(new SqlParameter("@rows", dataTable)
+        {
+            TypeName = "dbo.AddressType",
+            Value = dataTable
+        });
+        command.ExecuteNonQuery();
+
+
+
+        Console.WriteLine(timer.Elapsed.ToString());
+    }
+
+    private static void Main(string[] args)
+    {
+        AsyncMain().GetAwaiter().GetResult();
+    }
+}
